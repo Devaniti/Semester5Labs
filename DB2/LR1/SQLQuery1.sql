47d1bec63e9ad6e878bd12d7fb3f0c2dc7ea9951
Dima Bulatov <bestdarkstar@gmail.com>
2017-10-22 20:57:23 +0300

    lab1 db

--- DB2/LR1/SQLQuery1.sql
+++ DB2/LR1/SQLQuery1.sql
@@ -6,149 +6,289 @@ CREATE DATABASE LR1
 GO
 USE LR1;
 
-CREATE TABLE Accounts(
-	EmailAdress varchar(40) NOT NULL PRIMARY KEY
+CREATE TABLE Studio(
+	ID int NOT NULL PRIMARY KEY IDENTITY(1,1),
+	Country char(2) NOT NULL,
+	Name varchar(40) NOT NULL
 )
 
-CREATE TABLE Emails(
-	EmailText varchar(200) NOT NULL,
-	EmailAdress varchar(40) NOT NULL FOREIGN KEY REFERENCES Accounts(EmailAdress),
-	IsSpam BIT NOT NULL
+CREATE TABLE VHS(
+	ID int NOT NULL PRIMARY KEY IDENTITY(1,1),
+	Inventory_Number int NOT NULL UNIQUE,
+	Director varchar(40),
+	StudioID int FOREIGN KEY REFERENCES Studio(ID)
 )
 
-INSERT INTO Accounts VALUES ('admin@gmail.com'),('vasya@gmail.com'),('petia@gmail.com'),('mycoolemail@gmail.com');
-INSERT INTO Emails(EmailText,EmailAdress,IsSpam) VALUES 
-('Hi','admin@gmail.com',1),('Bye','admin@gmail.com',1),('I am Petya','admin@gmail.com',0),
-('Petya is bad','vasya@gmail.com',0),('Hi','admin@gmail.com',1),('Wow. Much Email. Very Text','mycoolemail@gmail.com',0),
-('You are bad','petia@gmail.com',0),('Covfefe','petia@gmail.com',1),('Not spam','admin@gmail.com',0),('Empty email','vasya@gmail.com',0),
-('Hi google','admin@gmail.com',0), ('You are good','vasya@gmail.com',0)
+CREATE TABLE Client(
+	ID int NOT NULL PRIMARY KEY IDENTITY(1,1),
+	LastName varchar(40),
+	Adress varchar(100),
+	Birthday varchar(100),
+	RegDate datetime DEFAULT(GetDate())
+)
+
+CREATE TABLE Rent(
+	ID int NOT NULL PRIMARY KEY IDENTITY(1,1),
+	StartDate DateTime NOT NULL,
+	ReturnDate DateTime DEFAULT(NULL),
+	VHS_ID int NOT NULL FOREIGN KEY REFERENCES VHS(ID),
+	ClientID int NOT NULL
+)
+
+INSERT INTO [dbo].[Client]
+           (LastName,Adress,Birthday)
+     VALUES
+           ('Pupkin Vitalii'   ,'Kiev'  ,'30 June'     ),
+           ('Petia Andrusuchn' ,'Moscow','15 July'     ),
+           ('Russel Gogh'      ,'Kiev'  ,'10 August'   ),
+           ('Andrew Laurence'  ,'Odessa','23 September'),
+		   ('Petia Lait'       ,'Kiev'  ,'12 December' )
+
+
+INSERT INTO [dbo].[Studio]
+           ([Country]
+           ,[Name])
+     VALUES
+           ('UA','UAFilms'    ),
+           ('RU','RUFilms'    ),
+           ('US','USFilms'    ),
+           ('US','USFilms2'   ),
+           ('LT','LithuaFilms')
+USE [LR1]
+GO
+
+INSERT INTO [dbo].[VHS]
+           ([Inventory_Number]
+           ,[Director]
+           ,[StudioID])
+     VALUES
+		(0         ,'Some Guy',            1),
+		(12376123  ,'Some Other Guy',      1),
+		(123761823 ,'Some Other Guy',      1),
+		(27612823  ,'Russia Guy',          2),
+		(27623823  ,'Russia Guy',          2),
+		(27622873  ,'Bad Director',        2),
+		(2321873   ,'Cool director',       3),
+		(23323813  ,'Cool director',       3),
+		(23323378  ,'Cool director',       3),
+		(23323323  ,'Cool director',       3),
+		(948323    ,'Other cool director', 4),
+		(94832323  ,'Third cool director', 4)
+
+USE [LR1]
+GO
+
+INSERT INTO [dbo].[Rent]
+           ([StartDate]
+           ,[ReturnDate]
+           ,[VHS_ID]
+           ,[ClientID])
+     VALUES
+		('2017-09-15 00:00:00.000',  '2017-09-20 00:00:00.000',  8 ,1  ),
+		('2016-10-17 00:00:00.000',  '2016-10-27 00:00:00.000',  1 ,3  ),
+		('2017-10-12 00:00:00.000',  '2017-10-21 00:00:00.000',  4 ,1  ),
+		('2017-10-22 00:00:00.000',  NULL,                       6 ,1  ),
+		('2017-10-20 00:00:00.000',  NULL,                       8 ,3  ),
+		('2016-10-11 00:00:00.000',  '2017-10-11 00:00:00.000',  3 ,1  ),
+		('2017-07-12 00:00:00.000',  '2017-08-02 00:00:00.000',  12,2  ),
+		('2017-10-13 00:00:00.000',  NULL,                       1 ,3  ),
+		('2016-10-14 00:00:00.000',  NULL,                       6 ,3  ),
+		('2017-10-15 00:00:00.000',  '2017-10-22 00:00:00.000',  3 ,3  ),
+		('2016-10-16 00:00:00.000',  '2016-10-17 00:00:00.000',  10,4  )
+
+
 
 -- 1.1 Використовуючи count() (або будь-яку іншу агрегатну функцію), partition
 -- by, order by та запит, що дасть такий самий результат, але не
 -- застосовуючи аналітичні функції.
 
--- Запити підраховують кількість емейлів користувачів
+-- Підраховує кількість різних адрес користувачів
 
-SELECT DISTINCT EmailAdress, COUNT(*) OVER (PARTITION BY EmailAdress) AS 'Email count' FROM Emails
-ORDER BY 'Email count' DESC
+SELECT DISTINCT Adress, COUNT(*) OVER (PARTITION BY Adress) AS 'Adress count' 
+FROM Client
+ORDER BY 'Adress count' DESC
 
-SELECT EmailAdress, COUNT(EmailText) AS 'Email count' FROM Emails
-GROUP BY EmailAdress
-ORDER BY 'Email count' DESC
+SELECT Adress, COUNT(Adress) AS 'Adress count' 
+FROM Client
+GROUP BY Adress
+ORDER BY 'Adress count' DESC
 
 -- 1.2 Використовуючи rank() або dense_rank(), partition by, order by та запит, що
 -- дасть такий самий результат, але не застосовуючи аналітичні функції.
 
--- Підраховує ранк кількості емейлів для емейлів які являються спамом та ні
+-- Підраховує ранк кількості замовлень клієнтів по студіям
 
-SELECT COUNT(EmailAdress) AS 'Emails count',IsSpam,RANK() OVER (PARTITION BY IsSpam ORDER BY COUNT(EmailAdress) DESC) AS 'Rank', EmailAdress FROM Emails
-GROUP BY EmailAdress,IsSpam
-ORDER BY IsSpam,COUNT(EmailAdress) DESC
+SELECT Client.LastName, COUNT(VHS.ID) AS 'VHS count', RANK() OVER (PARTITION BY VHS.StudioID ORDER BY COUNT(VHS.ID) DESC) AS 'Rank', Studio.Name AS 'Studio Name'
+FROM Client 
+JOIN Rent ON Rent.ClientID = Client.ID
+JOIN VHS ON VHS.ID = Rent.VHS_ID
+JOIN Studio ON Studio.ID = VHS.StudioID 
+GROUP BY Client.ID, Client.LastName, VHS.StudioID, Studio.Name
+ORDER BY VHS.StudioID,'VHS count' DESC
 
-SELECT COUNT(T.EmailText) AS 'Emails count',T.IsSpam,(SELECT COUNT(*)+1 FROM (SELECT COUNT(EmailText) q FROM Emails WHERE IsSpam = T.IsSpam GROUP BY EmailAdress HAVING COUNT(T.EmailText)<COUNT(EmailText)  ) e) AS 'Rank', EmailAdress FROM Emails AS T
-GROUP BY T.EmailAdress,T.IsSpam
-ORDER BY T.IsSpam,COUNT(T.EmailText) DESC
-
--- Ствроюю нову таблицю
-
-CREATE TABLE Users(
-	ID int NOT NULL PRIMARY KEY
-)
-
-CREATE TABLE Apps(
-	ID int NOT NULL PRIMARY KEY
-)
-
-CREATE TABLE Ratings(
-	UserID int NOT NULL FOREIGN KEY REFERENCES Users(ID),
-	AppID int NOT NULL FOREIGN KEY REFERENCES Apps(ID),
-	UIMark int NOT NULL,
-	UsabilityMark int NOT NULL,
-	RecomendMark int NOT NULL
-)
-
-INSERT INTO Users VALUES (1),(2),(3),(4),(5)
-INSERT INTO Apps VALUES (1),(2),(3),(4),(5)
-INSERT INTO Ratings VALUES (1,1,2,3,4),(1,2,4,4,5),(1,4,5,5,5),(1,5,3,1,5),(2,3,3,4,4),(3,2,2,3,4),(3,4,5,5,1),(4,1,4,4,3),(4,3,2,3,4),(5,2,5,4,3),(5,5,5,5,5)
+SELECT C.LastName, COUNT(V.ID) AS 'VHS count', 
+(SELECT COUNT(*)+1 FROM 
+(SELECT COUNT(V.ID) q 
+FROM Client 
+JOIN Rent ON Rent.ClientID = Client.ID
+JOIN VHS ON VHS.ID = Rent.VHS_ID
+WHERE VHS.StudioID = V.StudioID  GROUP BY ClientID HAVING COUNT(V.ID)<COUNT(VHS.ID)  )
+e) AS 'Rank',
+S.Name AS 'Studio Name'
+FROM Client AS C
+JOIN Rent AS R ON R.ClientID = C.ID 
+JOIN VHS AS V ON V.ID = R.VHS_ID
+JOIN Studio AS S ON S.ID = V.StudioID 
+GROUP BY C.ID, C.LastName, V.StudioID, S.Name
+ORDER BY V.StudioID,'VHS count' DESC
 
 -- 1.3 Використовуючи sliding window (rows), partition by, order by та запит, що дасть такий самий результат, але не застосовуючи аналітичні функції.
 
--- Виводить середню оцінку серед сусідніх оцінок
+-- Виводить кількість повернутих касет у суміжних замовленнях клієнта
 
 SELECT 
-    AppID,UserID,(UIMark+UsabilityMark+RecomendMark)/3.0 AS 'User AVG Mark',AVG((UIMark+UsabilityMark+RecomendMark)/3.0) OVER (PARTITION BY Ratings.AppID ORDER BY (UIMark+UsabilityMark+RecomendMark) ROWS BETWEEN 1 PRECEDING AND 1 FOLLOWING) AS 'Close AVG Mark'
-FROM Ratings
-ORDER BY AppID,UserID
+    ClientID,VHS_ID,StartDate,ReturnDate,COUNT(ReturnDate) OVER (PARTITION BY ClientID ORDER BY StartDate ROWS BETWEEN 1 PRECEDING AND 1 FOLLOWING) AS 'Close orders returned'
+FROM Rent
+ORDER BY ClientID, StartDate
 
 SELECT 
-    A.AppID,A.UserID,(A.UIMark+A.UsabilityMark+A.RecomendMark)/3.0 AS 'User AVG Mark',
-	(SELECT AVG(Mark) FROM
-	((SELECT TOP 1 (T.UIMark+T.UsabilityMark+T.RecomendMark)/3.0 AS Mark FROM Ratings AS T WHERE (T.UIMark+T.UsabilityMark+T.RecomendMark)>(A.UIMark+A.UsabilityMark+A.RecomendMark) AND T.AppID=A.AppID  ORDER BY (T.UIMark+T.UsabilityMark+T.RecomendMark) ASC)
+    ClientID,VHS_ID,StartDate,ReturnDate,
+	(SELECT COUNT(ReturnDate) FROM
+	((SELECT TOP 1 ReturnDate FROM Rent WHERE StartDate>R.StartDate AND ClientID=R.ClientID  ORDER BY StartDate ASC)
 	UNION ALL
-	(SELECT (A.UIMark+A.UsabilityMark+A.RecomendMark)/3.0)
+	(SELECT ReturnDate)
 	UNION ALL
-	(SELECT TOP 1 (T.UIMark+T.UsabilityMark+T.RecomendMark)/3.0 FROM Ratings AS T WHERE (T.UIMark+T.UsabilityMark+T.RecomendMark)<(A.UIMark+A.UsabilityMark+A.RecomendMark) AND T.AppID=A.AppID  ORDER BY (T.UIMark+T.UsabilityMark+T.RecomendMark) DESC))
+	(SELECT TOP 1 ReturnDate FROM Rent WHERE StartDate<R.StartDate AND ClientID=R.ClientID  ORDER BY StartDate DESC))
 	AS T)
-	AS 'Close AVG Mark'
-FROM Ratings AS A
-ORDER BY AppID,UserID
-
+	 AS 'Close orders returned'
+FROM Rent AS R
+ORDER BY ClientID, StartDate
 -- 1.4 Використовуючи sliding window (range) , partition by, order by та запит, що дасть такий самий результат, але не застосовуючи аналітичні функції.
 
--- Виводить кількість людей у яких середня оцінка меньша
+-- Виводить середній час повертання усіх минулих касет конкретним користувачем
 
 SELECT 
-    AppID,UserID,(UIMark+UsabilityMark+RecomendMark)/3.0 AS 'User AVG Mark',(COUNT(*) OVER (PARTITION BY Ratings.AppID ORDER BY (UIMark+UsabilityMark+RecomendMark) RANGE UNBOUNDED PRECEDING))-1 AS 'Lower Mark Count'
-FROM Ratings
-ORDER BY AppID,UserID
-
+    ClientID, VHS_ID, StartDate, ReturnDate,  
+	CASE 
+		WHEN ReturnDate IS NULL THEN NULL
+		ELSE (AVG(DATEDIFF(d,StartDate,ReturnDate)*1.0) OVER (PARTITION BY ClientID ORDER BY ReturnDate RANGE UNBOUNDED PRECEDING))
+	END  AS 'Awaiting VHS'
+FROM Rent
+ORDER BY ClientID,ISNULL(ReturnDate,GetDate())
 
 SELECT 
-    T.AppID,T.UserID,(T.UIMark+T.UsabilityMark+T.RecomendMark)/3.0 AS 'User AVG Mark',(SELECT COUNT(*) FROM Ratings AS A WHERE (T.AppID = A.AppID AND (T.UIMark+T.UsabilityMark+T.RecomendMark)>(A.UIMark+A.UsabilityMark+A.RecomendMark))) AS 'Lower Mark Count'
-FROM Ratings AS T
-ORDER BY AppID,UserID
-
--- Створю нову таблицю
-
-CREATE TABLE Playlist(
-	ID int NOT NULL PRIMARY KEY IDENTITY(1,1),
-	SongName varchar(40) NOT NULL
-)
-
-INSERT INTO Playlist VALUES
-	('I ll Face Myself -Battle-'),
-	('Triple Baka'),
-	('God knows'),
-	('The Battle for Everyone s Souls'),
-	('More Than One Heart'),
-	('Over My Head (Better Off Dead)'),
-	('No More'),
-	('Clair De Lune'),
-	('Through The Fire And Flames'),
-	('We are Number One'),
-	('Discord (The Living Tombstone Remix)')
-
+    ClientID, VHS_ID, StartDate, ReturnDate,  
+	CASE 
+		WHEN ReturnDate IS NULL THEN NULL
+		ELSE (SELECT AVG(DATEDIFF(d,StartDate,ReturnDate)*1.0) FROM Rent WHERE Rent.ClientID = R.ClientID AND Rent.ReturnDate <= R.ReturnDate)
+	END  AS 'Awaiting VHS'
+FROM Rent AS R
+ORDER BY ClientID,ISNULL(ReturnDate,GetDate())
 
 -- 1.5 Самостійно розібратися, як застосовується функція lag(). Використовуючи lag(), partition by, order by та запит, що дасть такий самий результат, але не застосовуючи аналітичні функції.
 
-SELECT SongName, LAG(SongName) OVER(ORDER BY ID) AS PreviousSong
-FROM Playlist
-ORDER BY ID
+-- Виводить скільки днів пройшло з попереднього прокату касети клієнтом
 
-SELECT SongName, (SELECT TOP(1) SongName FROM Playlist AS A WHERE A.ID<T.ID ORDER BY A.ID DESC) AS PreviousSong
-FROM Playlist AS T
-ORDER BY ID
+SELECT ClientID, Rent.StartDate, DATEDIFF(d,LAG(Rent.StartDate) OVER(PARTITION BY ClientID ORDER BY Rent.StartDate ASC),Rent.StartDate) AS 'Days from previous rent'
+FROM Rent
+ORDER BY ClientID,Rent.StartDate ASC
+
+SELECT ClientID, R.StartDate, DATEDIFF(d,
+(SELECT TOP(1) Rent.StartDate 
+FROM Rent
+WHERE Rent.StartDate<R.StartDate AND Rent.ClientID = R.ClientID ORDER BY Rent.StartDate DESC)
+,R.StartDate) AS 'Days from previous rent'
+FROM Rent AS R
+ORDER BY ClientID,R.StartDate ASC
 
 -- 1.6 Самостійно розібратися, як застосовується функція lead(). Використовуючи lead(), partition by, order by та запит, що дасть такий самий результат, але не застосовуючи аналітичні функції.
 
-SELECT SongName, LEAD(SongName) OVER(ORDER BY ID) AS NextSong
-FROM Playlist
-ORDER BY ID
+-- Виводить через скількі днів клієнт орендував наступну касету
 
-SELECT SongName, (SELECT TOP(1) SongName FROM Playlist AS A WHERE A.ID>T.ID ORDER BY A.ID ASC) AS NextSong
-FROM Playlist AS T
-ORDER BY ID
+SELECT Rent.ClientID, Rent.StartDate, DATEDIFF(d,Rent.StartDate,LEAD(Rent.StartDate) OVER(PARTITION BY ClientID ORDER BY Rent.StartDate ASC)) AS 'Days to next rent'
+FROM Rent
+ORDER BY Rent.ClientID, Rent.StartDate ASC
+
+SELECT R.ClientID, R.StartDate, DATEDIFF(d,R.StartDate,
+(SELECT TOP(1) Rent.StartDate 
+FROM Rent
+WHERE Rent.StartDate>R.StartDate AND Rent.ClientID = R.ClientID ORDER BY Rent.StartDate ASC)
+) AS 'Days to next rent'
+FROM Rent AS R
+ORDER BY R.ClientID, R.StartDate ASC
 
 -- 2.1 Змінити таблицю так, щоб вона не була у першій нормальній формі, навести цю таблицю у наповненому вигляді. Потім описати, які зміни треба зробити, щоб вона відповідала першій нормальній формі, навести цю таблицю у наповненому вигляді.
 
-CREATE TABLE 
+UPDATE Client
+SET Adress = Adress + ' Boryspil'
+-- Тепер у кожному клієнту (рядку) відповідає 2 адреси
+
+SELECT * FROM Client Order BY ID;
+-- Треба щоб залишилась тільки 1 відповідність на рядок
+
+UPDATE Client
+SET Adress = REPLACE(Adress,' Boryspil','')
+-- Тепер кожному рядку відповідає 1 адреса
+
+SELECT * FROM Client Order BY ID;
+
+-- 2.2 Описати, які зміни треба зробити, щоб таблиця відповідала другій нормальній формі, навести цю таблицю у наповненому вигляді.
+-- Треба додати іншу таблицю яка буде містити усі адреси(яких може бути декілька для 1го клієнта) і відповідні ID клієнтів та прибрати колонку Adress з таблиці Client
+
+SELECT (ID+0) AS ID, Adress
+INTO Adresses
+FROM Client
+
+INSERT INTO Adresses 
+SELECT ID, 'Boryspil'
+FROM Client
+
+ALTER TABLE Client
+DROP COLUMN Adress
+-- Тепер данні про адреси що можуть повторюватись зберігаються у окремій таблиці
+
+SELECT * 
+FROM Client 
+Order BY ID
+
+SELECT * 
+FROM Adresses
+Order BY ID
+
+-- 2.3 Описати, які зміни треба зробити, щоб таблиця відповідала третій нормальній формі, навести цю таблицю у наповненому вигляді.
+-- Треба винести усі колонки що залежать не тільки від первинного ключа у окремі таблиці
+
+SELECT LastName, Birthday
+INTO ClientBirthdays
+FROM Client
+
+ALTER TABLE Client
+DROP COLUMN Birthday
+-- Тепер нема колонок що залежать не тільки від первинного ключа
+
+SELECT * 
+FROM Client 
+Order BY ID
+
+SELECT * 
+FROM Adresses
+Order BY ID
+
+SELECT * 
+FROM ClientBirthdays
+Order BY LastName
+
+-- 2.4 Описати, які зміни треба зробити, щоб таблиця відповідала нормальній формі Бойса-Кода, навести цю таблицю у наповненому вигляді.
+-- Таблиця уже відповідає нормальній формі Бойса-Кода так як в ній не залишилось функціональних залежностей не винесених в окрему таблицю
+
+SELECT * 
+FROM Client 
+Order BY ID
+
+SELECT * 
+FROM Adresses
+Order BY ID
+
+SELECT * 
+FROM ClientBirthdays
+Order BY LastName

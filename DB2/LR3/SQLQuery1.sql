67056f7b80fbc5499c8e20753da4867dd8974a1c
Dima Bulatov <bestdarkstar@gmail.com>
2017-11-22 09:19:10 +0200

    db labs

--- DB2/LR3/SQLQuery1.sql
+++ DB2/LR3/SQLQuery1.sql
@@ -8,7 +8,8 @@ USE LR3
 GO
 
 CREATE TABLE Directors(
-	ID int PRIMARY KEY
+	ID int PRIMARY KEY,
+	Name varchar(40)
 )
 
 CREATE TABLE Director_mentors(
@@ -19,16 +20,16 @@ CREATE TABLE Director_mentors(
 
 -- task 1
 INSERT INTO Directors VALUES
-	(1),
-	(2),
-	(3),
-	(4),
-	(5),
-	(6),
-	(7),
-	(8),
-	(9),
-	(10)
+	(1, 'Gendalf'),
+	(2, 'Sauron'),
+	(3, 'R2D2'),
+	(4, 'Leonardo Di Caprio'),
+	(5, 'Luke Skywalker'),
+	(6, 'Filipp J. Fry'),
+	(7, 'Van Gogh'),
+	(8, 'Obi Wan Kenobi'),
+	(9, 'Mukha'),
+	(10, 'Superman')
 
 INSERT INTO Director_mentors VALUES
 	(1, NULL, 3   ),
@@ -62,8 +63,9 @@ AS
 		T.mentor_id = subquery.mentee_ids
    )
 
-   SELECT distinct mentee_ids
+   SELECT distinct mentee_ids, Directors.Name
    FROM subquery
+   JOIN Directors ON Directors.ID = mentee_ids
 GO
 
 EXEC listMentees 1 
@@ -89,8 +91,9 @@ AS
 		WHERE T.mentor_id IS NOT NULL
    )
 
-   SELECT distinct mentor_ids
+   SELECT distinct mentor_ids, Directors.Name
    FROM subquery
+   JOIN Directors ON Directors.ID = mentor_ids
 GO
 
 EXEC listMentors 6 
@@ -115,8 +118,9 @@ AS
 		T.mentor_id = subquery.mentee_ids
    )
 
-   SELECT distinct mentee_ids, lvl
+   SELECT distinct mentee_ids, lvl, Directors.Name
    FROM subquery
+   JOIN Directors ON Directors.ID = mentee_ids
 GO
 
 EXEC listMenteesWithLevel 1 
@@ -156,8 +160,9 @@ AS
 		WHERE iscycle = 0
    )
 
-   SELECT distinct mentee_ids, lvl
+   SELECT distinct mentee_ids, lvl, Directors.Name
    FROM subquery
+   JOIN Directors ON Directors.ID = mentee_ids
 GO
 
 EXEC listMenteesWithCycles 1
@@ -174,23 +179,26 @@ CREATE PROCEDURE listMenteesWithHistory
 AS 
    WITH subquery(mentee_ids, history, iscycle) AS 
    (
-		SELECT ID, 
-			CAST(CONCAT(@ID,'\',CAST(ID AS nvarchar(256))) AS nvarchar(256)), 0
+		SELECT Director_mentors.ID, 
+			CAST(CONCAT((SELECT Directors.Name FROM Directors WHERE ID = @ID),'\',CAST(Directors.Name AS nvarchar(256))) AS nvarchar(256)), 0
 		FROM Director_mentors
+		JOIN Directors ON Directors.ID = Director_mentors.ID
 		WHERE mentor_id = @ID
 		UNION ALL
 		SELECT 
 			T.ID,
-			CAST(CONCAT(subquery.history,'\',CAST(T.ID AS nvarchar(256))) AS nvarchar(256)),
+			CAST(CONCAT(subquery.history,'\',CAST(Directors.Name AS nvarchar(256))) AS nvarchar(256)),
 			(CASE WHEN T.mentor_id = subquery.mentee_ids THEN 1 ELSE 0 END)
 		FROM subquery
 		INNER JOIN Director_mentors AS T ON 
 		T.mentor_id = subquery.mentee_ids
+		JOIN Directors ON Directors.ID = T.ID
 		WHERE iscycle = 0
    )
 
-   SELECT mentee_ids, history
+   SELECT mentee_ids, history, Directors.Name
    FROM subquery
+   JOIN Directors ON Directors.ID = mentee_ids
 GO
 
 EXEC listMenteesWithHistory 1
